6b52bb3dbe590aed1baf29f9d4ae1054e07afd19
sipambi <sibandahmax@gmail.com>
2022-06-07 12:33:22 -0700

    Py Commit

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def add(a, b):
     """My addition function
-
     Args:
         a: first integer
         b: second integer
@@ -11,37 +10,34 @@ def add(a, b):
     """
     return (a + b)
 
+
 def sub(a, b):
     """My subtraction function
-
     Args:
         a: first integer
         b: second integer
-
     Returns:
         The return value. a - b
     """
     return (a - b)
 
+
 def mul(a, b):
     """My multiplication function
-
     Args:
         a: first integer
         b: second integer
-
     Returns:
         The return value. a * b
     """
     return (a * b)
 
+
 def div(a, b):
     """My division function
-
     Args:
         a: first integer
         b: second integer
-
     Returns:
         The return value. a / b
     """
