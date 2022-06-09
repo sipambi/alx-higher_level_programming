9ad8da1cf8cf2658eb2d2b29d8b446eb3ef56eef
sipambi <sibandahmax@gmail.com>
2022-06-09 12:59:44 -0700

    Py Commuts

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):                                                                      
-    newDict = sorted(a_dictionary)
+def print_sorted_dictionary(a_dictionary):
+    newDict = list(a_dictionary.keys())
+    newDict.sort()
     for i in newDict:
         print("{}: {}".format(i, a_dictionary.get(i)))
