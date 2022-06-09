98c113e5f2ab02297a61cf63fab15b977b8317b0
sipambi <sibandahmax@gmail.com>
2022-06-09 12:07:33 -0700

    Py Commits

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    sorted(a_dictionary)
-    for i in a_dictionary:
-        print("{}: {}".format(i, a_dictionary.get(i)))
+def print_sorted_dictionary(a_dictionary):                                                                      
+    newDict = sorted(a_dictionary)                                                                                          
+    for i in newDict:                                                                                                                  
+        print(i, a_dictionary.get(i)))
