55e7369782895bc9e2dfb43ba8c69ec41246772f
sipambi <sibandahmax@gmail.com>
2022-06-09 11:01:15 -0700

    Py Commits

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    theList = set(my_list)
     x = 0
-    for i in range(len(my_list)):
-        x = x + my_list[i]
+
+    for i in theList:
+        x = x + i
     return x
