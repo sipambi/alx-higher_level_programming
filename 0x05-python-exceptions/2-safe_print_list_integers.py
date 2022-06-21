8c95c4285a62c685aa19137a1f3f323f5ead8d02
sipambi <sibandahmax@gmail.com>
2022-06-21 08:51:54 -0700

    3rd Commit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             retVal += 1
         except (ValueError, TypeError):
             continue
-        print("")
-        return (retVal)
+    print("")
+    return(retVal)
