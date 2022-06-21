acee3b3b2dec40a411d8fa6768a269f82d9e7e03
sipambi <sibandahmax@gmail.com>
2022-06-21 08:23:51 -0700

    1st Commits

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     returnValue = 0
     for a in range(x):
         try:
-            print (my_list[a], end="")
+            print (f"{my_list[a]}", end="")
             returnValue = a + 1
         except IndexError:
             break
