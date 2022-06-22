4e0c13b9693b3cb27b24747bb3943797b4ef36c0
sipambi <sibandahmax@gmail.com>
2022-06-22 12:43:11 -0700

    3rd Commit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" class square with size defined by prev task"""
+
+
+class Square:
+    """ the class square"""
+
+    def __init__(self, size=0):
+        """ to construct the square with size """
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
