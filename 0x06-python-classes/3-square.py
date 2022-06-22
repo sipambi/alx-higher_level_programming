31578062de52f7b03a92d8e09bc0264fb3906f83
sipambi <sibandahmax@gmail.com>
2022-06-22 13:10:05 -0700

    getting tired

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,17 +3,17 @@
 
 
 class Square:
-        """ the class sqaure"""
-       
-       def __init__(self, size=0):
-           """ to construct the square with size """
-           if (type(size) is not int):
-               raise (TypeError("size must be an integer"))
-           elif (size < 0):
-               raise (ValueError("size must be >= 0"))
-           else:
-               self.__size = size
+    """ the class sqaure"""
 
-        def area(self):
-            """ method to get area of the shape(square)"""
-            return (self.__size ** 2)
+    def __init__(self, size=0):
+        """ to construct the square with size """
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
+
+    def area(self):
+        """ method to get area of the shape(square)"""
+        return (self.__size ** 2)
