bc17844448af16a6768c8bb42145d8a87a762d2b
sipambi <sibandahmax@gmail.com>
2022-06-22 12:26:33 -0700

    2nd Commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Class square with size private attribute """
+
+
+class Square:
+    """ class to create a square with size"""
+
+    def __init__(self, square_size):
+        """to construct the square"""
+        self.__size = square_size
