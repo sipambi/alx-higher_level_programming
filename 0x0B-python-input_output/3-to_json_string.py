b1f0f98cadf665b6603ddd6fb42cc3d1f2c3209d
sipambi <96469754+sipambi@users.noreply.github.com>
2022-07-05 21:34:54 +0200

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""
-    Module containing a Function thet returns the JSON representationn of an object
+""" Module that contains a function that returns the JSON
+representation of an object
 """
 import json
 
 
 def to_json_string(my_obj):
-    """ Returns JSON rep of an obj
-        Args:
-            my_obj: object to be represented
+    """ Function that returns the JSON representation of an object
+
+    Args:
+        my_obj: object
+
+    Raises:
+        Exception: when the object can't be encoded
+
     """
     return json.dumps(my_obj)
