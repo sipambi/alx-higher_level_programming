51970efb7f70440db2467a03bebac46087b733e6
sipambi <sibandahmax@gmail.com>
2022-07-05 11:44:51 -0700

    0 Commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module containing a file reading function """
+
+
+def read_file(filename=""):
+    """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'r', encoding = "utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+Files Input and Output
