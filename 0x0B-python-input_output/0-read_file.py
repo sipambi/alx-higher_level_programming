1edd61cc24cfb543ea517393f9962281bb67c4f0
sipambi <sibandahmax@gmail.com>
2022-07-05 11:50:19 -0700

    0 Commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-""" Module containing a file reading function """
+"""
+    Module containing a file reading function
+"""
 
 
 def read_file(filename=""):
     """ Function that reads from a file
-
-    Args:
-        filename: filename
-
-    Raises
-        Exception: when the file can be opened
-
+        Args:
+            filename: filename
     """
-
     with open(filename, 'r', encoding = "utf-8") as f:
         read_data = f.read()
         print(read_data, end='')
